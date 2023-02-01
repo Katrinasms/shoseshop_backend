0cf80ff47217a57421d4942de1195a529bb37501
kat <katrinashui1992@gmail.com>
2023-02-02 00:01:55 +0800

    try different cors and see4

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -53,9 +53,9 @@ userRoute.get(
 )
 
 
-userRoute.get('/', (req, res) => {
-  res.send('Hello user!')
-})
+// userRoute.get('/', (req, res) => {
+//   res.send('Hello user!')
+// })
 // Register
 userRoute.post(
     "/",
